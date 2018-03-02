fc26b884fca86dcbf51b7173b67629590dc1da8c
h1tan <axe.dota@mail.ru>
2018-03-02 20:31:48 +0200

    BOG

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,83 @@
 $(function() {
 
+    // 4. Пишем скрипт который создаст и отобразит карту Google Maps на странице.
+
+// Определяем переменную map
+    var map;
+
+// Функция initMap которая отрисует карту на странице
+    function initMap() {
+
+        // В переменной map создаем объект карты GoogleMaps и вешаем эту переменную на <div id="map"></div>
+        map = new google.maps.Map(document.getElementById('map'), {
+            // При создании объекта карты необходимо указать его свойства
+            // center - определяем точку на которой карта будет центрироваться
+            center: {lat: 55.760186, lng: 37.618711},
+            // zoom - определяет масштаб. 0 - видно всю платнеу. 18 - видно дома и улицы города.
+            zoom: 13,
+
+            // Добавляем свои стили для отображения карты
+            styles: [ { "featureType": "administrative", "elementType": "all", "stylers": [ { "visibility": "simplified" } ] }, { "featureType": "administrative.province", "elementType": "all", "stylers": [ { "visibility": "off" } ] }, { "featureType": "administrative.locality", "elementType": "all", "stylers": [ { "visibility": "simplified" } ] }, { "featureType": "administrative.neighborhood", "elementType": "all", "stylers": [ { "visibility": "off" } ] }, { "featureType": "landscape", "elementType": "all", "stylers": [ { "visibility": "on" } ] }, { "featureType": "landscape.man_made", "elementType": "all", "stylers": [ { "visibility": "on" } ] }, { "featureType": "landscape.man_made", "elementType": "geometry.fill", "stylers": [ { "lightness": "-2" }, { "hue": "#ffcc00" }, { "saturation": "0" } ] }, { "featureType": "landscape.natural", "elementType": "geometry.fill", "stylers": [ { "saturation": "9" }, { "lightness": "-5" } ] }, { "featureType": "poi", "elementType": "all", "stylers": [ { "visibility": "simplified" } ] }, { "featureType": "poi", "elementType": "labels.text", "stylers": [ { "hue": "#0024ff" } ] }, { "featureType": "poi", "elementType": "labels.text.stroke", "stylers": [ { "visibility": "on" }, { "lightness": "-100" } ] }, { "featureType": "poi", "elementType": "labels.icon", "stylers": [ { "visibility": "on" } ] }, { "featureType": "poi.attraction", "elementType": "all", "stylers": [ { "visibility": "off" } ] }, { "featureType": "poi.business", "elementType": "labels", "stylers": [ { "visibility": "simplified" } ] }, { "featureType": "poi.business", "elementType": "labels.text.fill", "stylers": [ { "color": "#111e6c" } ] }, { "featureType": "poi.business", "elementType": "labels.text.stroke", "stylers": [ { "visibility": "on" }, { "lightness": "-100" } ] }, { "featureType": "poi.business", "elementType": "labels.icon", "stylers": [ { "hue": "#0024ff" }, { "saturation": "-62" }, { "visibility": "simplified" } ] }, { "featureType": "poi.government", "elementType": "all", "stylers": [ { "visibility": "simplified" } ] }, { "featureType": "poi.medical", "elementType": "all", "stylers": [ { "visibility": "simplified" } ] }, { "featureType": "poi.park", "elementType": "geometry.fill", "stylers": [ { "saturation": "6" }, { "lightness": "-23" } ] }, { "featureType": "road", "elementType": "all", "stylers": [ { "visibility": "on" } ] }, { "featureType": "road", "elementType": "labels", "stylers": [ { "visibility": "off" } ] }, { "featureType": "transit", "elementType": "all", "stylers": [ { "visibility": "on" } ] }, { "featureType": "transit", "elementType": "labels", "stylers": [ { "visibility": "off" } ] }, { "featureType": "water", "elementType": "all", "stylers": [ { "visibility": "on" } ] }, { "featureType": "water", "elementType": "geometry", "stylers": [ { "color": "#12608d" } ] }, { "featureType": "water", "elementType": "geometry.fill", "stylers": [ { "lightness": "0" }, { "color": "#111e6c" } ] }, { "featureType": "water", "elementType": "labels.text.fill", "stylers": [ { "visibility": "off" } ] }, { "featureType": "water", "elementType": "labels.text.stroke", "stylers": [ { "visibility": "off" } ] } ]
+
+        });
+
+        // Создаем маркер на карте
+        var marker = new google.maps.Marker({
+
+            // Определяем позицию маркера
+            position: {lat: 55.762186, lng: 37.625711},
+
+            // Указываем на какой карте он должен появится. (На странице ведь может быть больше одной карты)
+            map: map,
+
+            // Пишем название маркера - появится если навести на него курсор и немного подождать
+            title: 'Наш маркер: Большой театр',
+
+            // Укажем свою иконку для маркера
+            icon: '../img/map-marker.svg'
+        });
+
+    }
+
+    try{
+        $(window).on('load', function() {
+            initMap();
+        });
+
+    } catch(er) {}
+
+
+	$(".hard-table .tr-default").click(function (e) {
+		$(this).next(".tr-content").toggle(); 
+	});
+
+	$(".progress-bar > span").each(function() {
+
+		var _thisChildrenSpan = $(this).children("span");
+		var statusText = _thisChildrenSpan.attr("statusText");
+
+		if ($(this).closest(".progress-bar").hasClass("green")) {
+			$(this)
+			.data("origWidth", $(this).width())
+			.width(0)
+			.animate({
+				width: $(this).data("origWidth")
+			}, 1200);
+
+			setTimeout(function() {
+				_thisChildrenSpan.text(statusText)
+			}, 1200);
+		}else{
+			_thisChildrenSpan.text(statusText)
+		}
+		
+
+		
+
+		
+	});
+
+
 
     //  active currency class
     $(".exchange .column .subblock-content .btns .item").click(function (e) {
@@ -138,6 +216,49 @@ $(function() {
 
 	
 
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	/*
+ * Replace all SVG images with inline SVG
+ */
+ $('img.img-svg').each(function(){
+ 	var $img = $(this);
+ 	var imgID = $img.attr('id');
+ 	var imgClass = $img.attr('class');
+ 	var imgURL = $img.attr('src');
+
+ 	$.get(imgURL, function(data) {
+        // Get the SVG tag, ignore the rest
+        var $svg = $(data).find('svg');
+
+        // Add replaced image's ID to the new SVG
+        if(typeof imgID !== 'undefined') {
+        	$svg = $svg.attr('id', imgID);
+        }
+        // Add replaced image's classes to the new SVG
+        if(typeof imgClass !== 'undefined') {
+        	$svg = $svg.attr('class', imgClass+' replaced-svg');
+        }
+
+        // Remove any invalid XML tags as per http://validator.w3.org
+        $svg = $svg.removeAttr('xmlns:a');
+
+        // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+        if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+        	$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+        }
+
+        // Replace image with new SVG
+        $img.replaceWith($svg);
+
+    }, 'xml');
+
+ });
 
 
 });
